529bd7e06fb83b419d80d65ccf3a3c4975e71d8a
ArC48 <beqa3352@gmail.com>
2024-05-18 01:10:01 +0400

    pages setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { Component } from 'react'
 import './App.css';
+import Default from './pages/Default';
+import Favorites from './pages/Favorites';
+import Home from './pages/Home';
+import SingleRecipe from './pages/SingleRecipe';
+
 
 function App() {
   return (
     <div>
-      Welcome to IngreDientia!
+      <Home />
+      <SingleRecipe />
+      <Favorites />
+      < Default />
     </div>
   );
 }
